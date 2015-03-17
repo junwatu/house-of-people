049cf180996d751e434cf3c9408ec8f3c0ae96b4
Equan Pr <byte@equan.me>
2015-03-17 18:14:53 +0700

    add test for CRUD houses API

--- test/test.js
+++ test/test.js
@@ -6,7 +6,13 @@ var assert = require('assert');
 var supertest = require('supertest');
 var api = supertest('http://localhost:3000/api');
 
+
 describe('House API', function(){
+
+   var house = {
+       "address": "Acme Street", 
+       "id":1
+   };
 
    describe('GET /houses', function(){
       it('should return 200', function(done){
@@ -18,12 +24,24 @@ describe('House API', function(){
    });
 
    describe('POST /house', function(){
-
-   		var houses = {"address": "Acme Street"};
-
-   		it('should return 200', function(done){
-   			api.post('/houses').send(houses).expect('Content-Type',/json/).expect(200, done);
-   		})
+      it('should return 200', function(done){
+   		   api.post('/houses').send(house).expect('Content-Type',/json/).expect(200, done);
+   	   })
    });
+
+   describe('PUT /house/{id}', function(done){
+      var house2 = {"address": "Acme 2 Street"};
+
+      it('should return the changed data with id 1', function(done){
+           api.put('/houses/1').send(house2).expect({"address":"Acme 2 Street", "id":1}).expect(200, done);
+      });
+   });
+
+   describe('DEL /house/{id}', function(done){
+   	  it('should delete data with id 1', function(){
+           api.del('/houses/1').expect(204, done);
+   	  });
+   });
+
 
 });
